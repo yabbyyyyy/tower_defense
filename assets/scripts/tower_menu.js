2bfd6f089047694a8177a08b31dc9199c20049ad
chao1009 <thor1009@gmail.com>
2020-07-15 22:55:09 -0500

    add event blocker to the menus

--- assets/scripts/tower_menu.js
+++ assets/scripts/tower_menu.js
@@ -9,7 +9,21 @@ cc.Class({
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+        // block it to the level node
+        this.node.on(cc.Node.EventType.TOUCH_START, (event) => {
+            // the menu is in ellipse shape
+            let canvas = cc.director.getScene().getChildByName('Canvas');
+            let diff = event.touch.getLocation()
+                       .sub(cc.v2(canvas.width/2., canvas.height/2.))
+                       .sub(this.node.position);
+            let a = this.node.width/2.*this.node.scale;
+            let b = this.node.height/2.*this.node.scale;
+            if (diff.x*diff.x/a/a + diff.y*diff.y/b/b < 1.) {
+                event.stopPropagation(); 
+            }
+        });
+	},
 	
 	buttonClick: function (event, customData) {
 		// cc.log(customData);
